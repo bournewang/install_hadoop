010a1c6b7a5b1794192471efc938f734f7e79ae8
Bourne <68225384@qq.com>
2022-04-11 16:19:41 +0800

    update download.sh;

--- downloads.sh
+++ downloads.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-wget https://downloads.apache.org/hive/hive-3.1.2/apache-hive-3.1.2-bin.tar.gz
+mkdir software
+cd software
+echo "downloading packages"
 wget https://mirrors.aliyun.com/apache/hive/hive-3.1.2/apache-hive-3.1.2-bin.tar.gz
 wget https://mirrors.aliyun.com/apache/zookeeper/zookeeper-3.5.9/apache-zookeeper-3.5.9-bin.tar.gz
 wget https://mirrors.aliyun.com/apache/hadoop/common/hadoop-2.10.1/hadoop-2.10.1.tar.gz
-wget https://mirrors.aliyun.com/apache/spark/spark-2.4.8/spark-2.4.8-bin-hadoop2.7.tgz
 wget https://mirrors.aliyun.com/apache/kafka/2.8.1/kafka_2.12-2.8.1.tgz
+wget https://mirrors.aliyun.com/apache/spark/spark-2.4.8/spark-2.4.8-bin-hadoop2.7.tgz
 wget http://archive.apache.org/dist/sqoop/1.4.7/sqoop-1.4.7.bin__hadoop-2.6.0.tar.gz
